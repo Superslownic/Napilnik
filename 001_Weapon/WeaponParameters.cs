9456aaf4ab295bb0fe5eaf306bf93e7fe78acc9a
Superslownic <isuperslownic@gmail.com>
2021-07-06 22:25:45 +0700

    Added the solution of the first task

---
+++ 001_Weapon/WeaponParameters.cs
@@ -0,0 +1,12 @@
+﻿namespace NapilnikTask1
+{
+    public class WeaponParameters
+    {
+        public int Damage { get; private set; }
+
+        public WeaponParameters(int damage)
+        {
+            Damage = damage;
+        }
+    }
+}
